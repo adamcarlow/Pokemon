e7539685aec3872d36a5f950092333e0e98c8577
AC <63122442+adamcarlow@users.noreply.github.com>
2022-09-26 20:38:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Pokemon Terminal game for Code Academy CS101 Final Project
+
+class Pokemon():
+
+  def __init__(self, type, attack, defense, weakness):
+    self.type = type
+    self.attack = attack
+    self.defense = defense
+    self.weakness = weakness
+
+  def battle(self, attack)
+
+  
+Charizard = Pokemon('Fire',120,275,'Water')
+Snorlax = Pokemon("Colorless",90,350,'Fighting')
+
+print(Charizard.type)
+print(Charizard.attack)
+print(Charizard.defense)
+print(Charizard.weakness)
