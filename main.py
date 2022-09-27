e5156c7f924ae883310698fcb69919e4e63f421a
AC <63122442+adamcarlow@users.noreply.github.com>
2022-09-27 16:20:29 +0000

    Changed battle logic, added blank prints for spacing, coinflip for who goes 1st or 2nd

--- main.py
+++ main.py
@@ -10,41 +10,51 @@ class Pokemon():
     self.name = name
 
     
-  def battle(player1, player2):
-    while player2.defense > 0: 
-      player1_attack = round((player1.attack * random.random()),0)
-      player2.defense = player2.defense - player1_attack
-      if player2.defense > 0:
-        print("{p} does ".format(p=player1.name) + str(player1_attack) + ' damage to {p2}!'.format(p2=player2.name))
-        print('{p2} has {d2} life left!'.format(p2=player2.name,d2=player2.defense))
+  def battle(first, second):
+    while first.defense > 0 and player2.defense > 0:
+      first_attack = round((first.attack * random.random()),0)
+      second.defense = second.defense - first_attack
+      second_attack = round((second.attack * random.random()),0)
+      first.defense = first.defense - second_attack
+      if second.defense > 0:
+        print("{p} does ".format(p=player1.name) + str(first_attack) + ' damage to {p2}!'.format(p2=second.name))
+        print('{p2} has {d2} life left!'.format(p2=second.name,d2=second.defense))
       else:
-        print("{p} does ".format(p=player1.name) + str(player1_attack) + ' damage to {p2}!'.format(p2=player2.name))
-        print('{p2} has been defeated!'.format(p2=player2.name,d2=player2.defense))
+        print("{p} does ".format(p=player1.name) + str(first_attack) + ' damage to {p2}!'.format(p2=second.name))
+        print('{p2} has been defeated!'.format(p2=second.name,d2=second.defense))
+    #while player1.defense > 0: 
+      print('')
+      if first.defense > 0:
+        print("{p} does ".format(p=second.name) + str(second_attack) + ' damage to {p1}!'.format(p1=first.name))
+        print('{p1} has {d1} life left!'.format(p1=first.name,d1=first.defense))
+      else:
+        print("{p} does ".format(p=second.name) + str(second_attack) + ' damage to {p1}!'.format(p1=first.name))
+        print('{p1} has been defeated!'.format(p1=first.name,d1=first.defense))
+      print('')
     
 
 #print("Creating circle with diameter {d}".format(d=diameter)) 
-Charizard = Pokemon('Fire',120,275,'Water','Charizard')
-Snorlax = Pokemon("Colorless",90,350,'Fighting','Snorlax')
+Charizard = Pokemon('Fire',90,275,'Water','Charizard')
+Snorlax = Pokemon("Colorless",45,350,'Fighting','Snorlax')
+Mew = Pokemon("Psychic",75,240,'Darkness','Mew')
 
-characters = [Charizard, Snorlax]
-#input1 = input("Which character will you choose player1? ")
-#input2 = input("Which character will you choose player2? ")
+characters = [Charizard, Snorlax, Mew]
+
+
 player1 = random.choice(characters)
+player1_name = input('Player1 what is your name?  ')
 player2 = random.choice(characters)
-print('Player1 you will be ' + player1.name)
-print('Player2 you will be ' + player2.name)
-#print(Charizard.type)
-#print(Charizard.attack)
-#print(Charizard.defense)
-#print(Charizard.weakness)
-#print(Snorlax.weakness)
+player2_name = input('Player2 what is your name?  ')
+print('')
+print(player1_name + ' you will be ' + player1.name + '!')
+print('')
+print(player2_name + ' you will be ' + player2.name + '!')
+print('')
+
+players = [player2,player1]
+first = random.choice(players)
+players2 = players.remove(first)
+second = random.choice(players)
+
 battle1 = Pokemon.battle(player1,player2)
-battle2 = Pokemon.battle(player1,player2)
-battle3 = Pokemon.battle(player1,player2)
-battle4 = Pokemon.battle(player1,player2)
-battle5 = Pokemon.battle(player1,player2)
-battle6 = Pokemon.battle(player1,player2)
-#print(Snorlax.type)
-#print(Snorlax.attack)
-#print(Snorlax.defense)
-#print(Snorlax.weakness)
+
